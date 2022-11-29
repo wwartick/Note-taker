2b4417378099e33ae7cf7038b246a506564e0de0
wwartick <willwartick@gmail.com>
2022-11-29 15:09:31 -0800

    get/post/validate notes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { createNewNote, validateNote, deleteNote } = require('../../lib/notes.js');
+const { notes } = require('../../data/db.json');
+
+router.get('/notes', (req, res) => {
+    // return all notes
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    //assign id by location in array
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('Note needs a title and text');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
